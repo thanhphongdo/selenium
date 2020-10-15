3e62426a965397e946c13c466919e5401412349c
Phong Do <phong.do@zaitenllc.com>
2020-10-15 17:06:26 +0700

    expect result

--- main.js
+++ main.js
@@ -9,25 +9,3 @@ async function start() {
 
 }
 start();
-
-// const {assert, expect, should} = require('chai');
-
-// let foo = 'bar2'
-// let beverages = {tea: ['chai', 'matcha', 'oolong']};
-// try {
-//     // assert.typeOf(foo, 'string'); // without optional message
-//     assert.typeOf(foo, 'string', 'foo is a string'); // with optional message
-//     assert.include(foo, 'ar2', 'include "ar"')
-//     // // assert.equal(foo, 'bar2', 'foo equal `bar`');
-//     // assert.lengthOf(foo, 3, 'foo`s value has a length of 3');
-//     // assert.lengthOf(beverages.tea, 3, 'beverages has 3 types of tea');
-
-//     // expect(foo, 'foo should is a string').to.be.a('bool');
-//     // expect(foo).to.equal('bar');
-//     // expect(foo).to.have.lengthOf(3);
-//     // expect(beverages).to.have.property('tea').with.lengthOf(3);
-    
-// }
-// catch (err) {
-//     console.log(err.message);
-// }
